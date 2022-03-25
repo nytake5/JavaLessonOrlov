45c1d889cea07b0e0c92993996542b653c53b678
Orlov Daniil <58112678+nytake5@users.noreply.github.com>
2022-03-25 09:23:42 +0400

    Add files via upload

---
+++ JavaLessonOrlov/src/ssu/daniil_orlov/java/lesson_2/task_1/food/Fruit.java
@@ -0,0 +1,7 @@
+package ssu.daniil_orlov.java.lesson_2.task_1.food;
+
+public class Fruit extends Ingredient {
+    public Fruit(String title, TypeOfCooking type, Condition condition, int count, int calories){
+        super(title, type, condition, count, calories);
+    }
+}
